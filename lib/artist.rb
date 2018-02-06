acef27cf2fe6a3066df7943984610a86d8c94d54
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-06 18:52:45 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,10 +21,6 @@ class Artist
     self.class.all << self
   end
 
-  def self.create(name)
-    new(name).tap{ |a| a.save }
-  end
-
   def add_song(song)
     song.artist = self unless song.artist
     songs << song unless songs.include?(song)
